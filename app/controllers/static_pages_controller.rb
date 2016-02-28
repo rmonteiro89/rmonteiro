c31dcaeca86cea01310d137de046042cfcb9663d
Raphael Monteiro <rmonteiro89@hotmail.com>
2016-02-28 21:29:38 +0100

    Refactoring controller to rescue errors from instagram

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,13 @@
 class StaticPagesController < ApplicationController
+
   def home
-    @instagram = ::Instagram.user_recent_media("1974526845", {:count => 9})
+    @instagram = instagram_recente_media
+  end
+
+  private
+  def instagram_recente_media
+    ::Instagram.user_recent_media("1974526845", {:count => 9})
+  rescue
+    []
   end
 end
