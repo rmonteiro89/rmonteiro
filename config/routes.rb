b50fa79a0fc6f188a0fd8378dbf3bdf4f44f406b
Raphael Monteiro <rmonteiro89@hotmail.com>
2016-03-20 17:28:43 +0100

    Add download resume

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
+  get 'resume' => 'static_pages#resume', as: :resume
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
