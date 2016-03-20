f7d1c981156d64c5765af2523213bc5ae0e5ee16
Raphael Monteiro <rmonteiro89@hotmail.com>
2016-03-20 17:28:43 +0100

    Add download resume

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,14 @@ class StaticPagesController < ApplicationController
     @instagram = instagram_recente_media
   end
 
+  def resume
+    send_file(
+      "#{Rails.root}/public/resume.pdf",
+      filename: 'raphael_monteiro_resume_2016.pdf',
+      type: 'application/pdf'
+    )
+  end
+
   private
   def instagram_recente_media
     ::Instagram.user_recent_media("1974526845", {:count => 9})
